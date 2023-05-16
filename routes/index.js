08966a358acac6d4ba15121df747c237f8a782e0
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-05-16 19:56:24 +0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const user = require('../controllers/user');
 
 const middlewares = require('../utils/middlewares');
 
+router.get('/home', async (req, res, next) => {
+  return res.status(200).json({
+		status: true,
+		message: 'welcome to coba Ci CD API',
+		data: null,
+	});
+});
+
 router.post('/auth/register', user.register);
 router.post('/auth/login', user.login);
 router.get('/auth/whoami', middlewares.auth, user.whoami);
